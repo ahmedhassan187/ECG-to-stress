e355f140b7c8fb523d3491722270ccdf0672ab7e
ahmedhassan187 <ahmedhassann187@gmail.com>
2026-06-15 16:43:36 +0200

    trail to make gitignore

---
+++ __th__.py
@@ -0,0 +1,79 @@
+# Data file
+def read_subject(file_path):
+    return ecg,label
+def read_dataset(folder_path):
+    for i in m:
+        read_subject()
+    return ecgs, labels
+def get_label_sub(ecg,label):
+    return ecg
+def get_label_dataset(ecgs,labels):
+    for i in m:
+        get_label_sub
+    return ecgs
+
+def get_random_chunck(long_ecg):
+    return short_ecg
+def get_chunck_ecg(long_ecg):
+    short_ecgs = []
+    for i in m:
+        short_ecgs.append(chunck)
+    return short_ecgs
+
+# Visualization file
+def plot_features_dist(fetaures):
+    return 
+def plot_features_corr_mat(fetaures):
+    return 
+def plot_results(df):
+    return 
+def plot_conf_mat(conf_mat):
+    return 
+# Features file
+def get_mean_rr(ecgs):
+    return mean_rr
+def get_mean_hr(ecgs):
+    return mean_hr
+def get_rmssd(ecgs):
+    return rmssd 
+def get_sdnn(ecgs):
+    return sdnn
+def get_pnn50(ecgs):
+    return pnn50
+def get_lf_power(ecgs):
+    return lf_power
+def get_hf_power(ecgs):
+    return hf_power
+def get_hf_lf_ratio(ecgs):
+    return hf_lf_ratio
+def get_hrv_features():
+    get_mean_rr()
+    get_mean_hr()
+    get_lf_power()
+    get_hf_power()
+    get_hf_lf_ratio()
+    get_pnn50()
+    return features
+
+def get_dataframe(features,labels):
+
+    return df_features
+
+
+# Comparing file 
+def get_r(features1,features2):
+    return r
+def get_icc(features1,features2):
+    return icc
+def get_mae(features1,features2):
+    return mae
+
+# ML file 
+def loso_evaluate(data,model):
+    return mat
+def eval_all(features,models):
+    return results
+def make_conf_mat(features,model):
+    return mat
+
+# FFT File
